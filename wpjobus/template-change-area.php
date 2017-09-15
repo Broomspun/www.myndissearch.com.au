06492f0edc7a61ee597cc68279d62cb59bd57833
broomspun1208 <admin@storm-east.com>
2017-09-15 06:10:22 -0500

    Assigend areas based on the postcode extracted from company address.

--- wpjobus/template-change-area.php
+++ wpjobus/template-change-area.php
@@ -7,6 +7,26 @@ $page = get_page($post->ID);
 $td_current_page_id = $page->ID;
 
 add_filter('remove_parent_areas', 'remove_parent_areas_callback',10,1);
+add_filter('filter_areas_with_the_zipcode', 'filter_areas_with_the_zipcode_callback',10,2);
+
+function filter_areas_with_the_zipcode_callback($areas, $zipcode){
+    $new_areas = array();
+    foreach ($areas as $area){
+        if($area->parent==0) continue;
+        $id = $area->term_id;
+        $key = "taxonomy_term_$id";
+
+        $postcode = get_option($key)['postcodes'];
+
+        if(strstr($postcode,$zipcode)==false) continue;
+
+        $new_areas[] = $area;
+
+        }
+
+        return $new_areas;
+}
+
 function remove_parent_areas_callback($areas){
     $new_areas = array();
     foreach ($areas as $area){
@@ -61,15 +81,67 @@ get_header();
         }
 
         $newPostcode['postcodes'] = implode(',', $newCodes);
-            update_option($key, $newPostcode);
+        update_option($key, $newPostcode);
 
         echo $area->name.' => '.$newPostcode['postcodes'].'<br/>';
     }
+
+
+    $query = new WP_Query( array(
+            'post_type'         =>  'company',
+            'posts_per_page'    =>  10,
+            'post_status'       =>  'publish'
+        ) );
 ?>
 
 	<section id="blog">
 
 		<div class="container">
+            <?php
+            global $post;
+            if ( $query->have_posts() ) : ?>
+                <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+                    <div class="entry">
+                        <?php
+
+                        $address = get_post_meta($post->ID, 'wpjobus_company_address',true);
+                        $zipcode = -1;
+                        if(preg_match("/(\d{4})$/", $address, $matches)){
+                            $zipcode = $matches[0];
+                        };
+                        if($zipcode==-1) continue;
+                        ?>
+                        <h2 class="title"><?php the_title(); ?></h2>
+                        <h4><?php echo 'zipcode => ',$zipcode; ?></h4>
+                        <?php
+                        //find areas in the zip code
+                        $areas = get_terms(
+                            array(
+                                'taxonomy' => 'area',
+                                'hide_empty' => false,
+                            )
+                        );
+
+                        $areas = apply_filters('filter_areas_with_the_zipcode', $areas, $zipcode);
+
+                        $terms = array();
+                        if(!empty($areas)){
+                            foreach ($areas as $area) {
+                                echo $area->name . ', ';
+                                $terms[] = $area->term_id;
+                            }
+                        }
+                        echo '<br/>';
+
+                        wp_set_post_terms($post->ID,$terms, 'area');
+                        ?>
+
+                    </div>
+                <?php endwhile; wp_reset_postdata(); ?>
+                <!-- show pagination here -->
+            <?php else : ?>
+                <!-- show 404 error here -->
+            <?php endif; ?>
 
 		</div>
 
